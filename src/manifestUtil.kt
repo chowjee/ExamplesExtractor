98c5e19f72b356cc54216a5be70dc75853492ca1
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2017-04-14 23:44:57 +0300

    Reformat generated json

--- src/manifestUtil.kt
+++ src/manifestUtil.kt
@@ -2,45 +2,45 @@ package manifestUtil
 
 fun manifestForExercisesFolder(range: IntRange) =
 """{
-    "sequential": false,
-    "examples": [
-        ${range.map { "Exercise$it" }.asStringsInJson()}
-    ]
+  "sequential": false,
+  "examples": [
+    ${range.map { "Exercise$it" }.asStringsInJson()}
+  ]
 }"""
 
 fun manifestForExercise() =
 """{
-    "confType": "junit"
+  "confType": "junit"
 }"""
 
 fun manifestForExamples(name: String, examples: List<String>) =
 """{
-    "name" : "$name",
-    "files" : [ ${examples.joinToString(transform = ::fileInfo)} ],
-    "args" : "",
-    "searchForMain": false,
-    "confType" : "java"
+  "name" : "$name",
+  "files" : [ ${examples.joinToString(transform = ::fileInfo)} ],
+  "args" : "",
+  "searchForMain": false,
+  "confType" : "java"
 }"""
 
 private fun fileInfo(name: String) =
 """{
-        "filename" : "$name",
-        "modifiable" : "true"
-    }"""
+    "filename" : "$name",
+    "modifiable" : "true"
+  }"""
 
 
 fun topLevelManifest(chapters: List<String>) =
 """{
-    "folders": [
-        ${chapters.asStringsInJson()}
-    ]
+  "folders": [
+    ${chapters.asStringsInJson()}
+  ]
 }"""
 
 fun manifestForExamplesFolder(name: String) =
 """{
-    "name" : "$name",
-    "folder" : "$name",
-    "examples" : [ "Examples" ]
+  "name" : "$name",
+  "folder" : "$name",
+  "examples" : [ "Examples" ]
 }"""
 
-private fun List<String>.asStringsInJson() = joinToString(",\n        ") { "\"$it\"" }
+private fun List<String>.asStringsInJson() = joinToString(",\n    ") { "\"$it\"" }

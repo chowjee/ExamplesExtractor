7d0251713b873166327ce988e3e9c00969f45922
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2017-07-17 17:16:10 +0200

    Rename to make placeholders work in edu course

--- src/edu/courseFormat.kt
+++ src/edu/courseFormat.kt
@@ -8,39 +8,45 @@ data class Course(
 
 data class Lesson(val id: Int, val title: String, val task_list: List<Task>)
 
-interface Task
+interface Task {
+    val name: String
+    val stepic_id: Int
+    val task_files: Map<String, TaskFile>
+    val task_texts: Map<String, String>
+    val task_type: String
+}
 
 data class GeneralTask(
-        val name: String,
-        val stepic_id: Int,
-        val task_files: Map<String, TaskFile>,
+        override val name: String,
+        override val stepic_id: Int,
+        override val task_files: Map<String, TaskFile>,
         val test_files: Map<String, String>,
-        val task_texts: Map<String, String>,
-        val task_type: String = "pycharm"): Task
+        override val task_texts: Map<String, String>,
+        override val task_type: String = "pycharm") : Task
 
 data class TheoryTask(
-        val name: String,
-        val stepic_id: Int,
-        val task_files: Map<String, TaskFile>,
-        val task_texts: Map<String, String>,
-        val task_type: String = "theory"): Task
+        override val name: String,
+        override val stepic_id: Int,
+        override val task_files: Map<String, TaskFile>,
+        override val task_texts: Map<String, String>,
+        override val task_type: String = "theory") : Task
 
 data class TaskFile(
-    val name: String,
-    val text: String,
-    val placeholders: List<Placeholder>
+        val name: String,
+        val text: String,
+        val placeholders: List<Placeholder>
 )
 
 data class Placeholder(
-    val offset: Int,
-    val length: Int,
-    val subtask_infos: Map<String, SubTaskInfo>
+        val offset: Int,
+        val length: Int,
+        val subtask_infos: Map<String, SubTaskInfo>
 )
 
 data class SubTaskInfo(
-    val hints: List<String>,
-    val possibleAnswer: String,
-    val placeholder_text: String,
-    val has_frame: Boolean = true,
-    val need_insert_text: Boolean = false
+        val hints: List<String>,
+        val possible_answer: String,
+        val placeholder_text: String,
+        val has_frame: Boolean = true,
+        val need_insert_text: Boolean = false
 )

38fa7dc63fccd473ba8c56295e4e8cafeb52ae3e
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2017-08-09 11:07:49 +0200

    Average for a range

--- src/chapterLength/countAverageLength.kt
+++ src/chapterLength/countAverageLength.kt
@@ -3,7 +3,9 @@ package chapterLength
 import atoms.Atoms
 
 fun main(args: Array<String>) {
-  val atomSizes = Atoms().atomInfoList.associate { it.name to it.markdownFile.readLines().size }
-  println("average: " + atomSizes.values.toList().average())
+  val atomSizes = Atoms().atomInfoList.associate { it.name.substring(0, 2).toInt() to it.markdownFile.readLines().size }
   println(atomSizes)
+  println("average: " + atomSizes.values.toList().average())
+  val range = 6..17
+  println("average for $range: " + range.map { atomSizes.getValue(it) }.toList().average())
 }
